7be217f3a0e1ccf373babdcdd2fc9cc246838a88
Rhea Goyal <goyal42@purdue.edu>
2019-04-21 19:09:58 -0400

    planner page modifications

---
+++ plannerFunc.js
@@ -0,0 +1,13 @@
+
+function getTask(){
+    var task = document.getElementById("task").value;
+    console.log(task);
+    document.getElementById("todo").innerHTML= task ;
+
+}
+
+function showUrgent(){
+    var myPopup = window.open("", "", "width=200, height=100");
+}
+
+
